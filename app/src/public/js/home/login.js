8a64693d1a447cf6eee765811625db5f56827947
jeehwan <tjtojan@naver.com>
2022-03-06 23:13:01 +0900

    돔을 이용하여 html 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,2 +1,14 @@
-console.log('hello');
-console.log('bye');
+const id = document.querySelector("#id");
+const psword = document.querySelector("#psword");
+const loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click", login);
+
+function login() {
+    const req = {
+        id: id.value,
+        psword: psword.value,
+    };
+
+    console.log(req);
+}
